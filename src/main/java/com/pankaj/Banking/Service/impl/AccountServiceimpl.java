cd0fc1298a70621b8faf0845dbcd1fc89bfd1de5
Pankaj <upadhyaypankaj2004@gmail.com>
2025-03-05 17:23:21 +0530

    BAnk_app

--- src/main/java/com/pankaj/Banking/Service/impl/AccountServiceimpl.java
+++ src/main/java/com/pankaj/Banking/Service/impl/AccountServiceimpl.java
@@ -5,9 +5,6 @@ import com.pankaj.Banking.Service.AccountService;
 import com.pankaj.Banking.dto.AccountsDto;
 import com.pankaj.Banking.model.Account;
 import com.pankaj.Banking.repo.AccountRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
---
+++ src/main/java/com/pankaj/Banking/model/Users.java
@@ -0,0 +1,20 @@
+package com.pankaj.Banking.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Users {
+    @Id
+    private int userid;
+    private String username;
+    private String password;
+}
